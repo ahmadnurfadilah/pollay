a75271e7abf7895601368722b22e877036e77487
ahmadnurfadilah <ahmadnurfadilah22@gmail.com>
2025-02-07 00:34:37 +0700

    add wallet connection & basic ui

---
+++ app/provider.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createAppKit } from '@reown/appkit/react'
+import { polygon } from '@reown/appkit/networks'
+import React, { type ReactNode } from 'react'
+import { cookieToInitialState, WagmiProvider, type Config } from 'wagmi'
+import { projectId, wagmiAdapter } from './config'
+
+// Set up queryClient
+const queryClient = new QueryClient()
+
+if (!projectId) {
+  throw new Error('Project ID is not defined')
+}
+
+// Set up metadata
+const metadata = {
+  name: 'pollay',
+  description: 'Pollay',
+  url: 'https://pollay.chat',
+  icons: ['https://avatars.githubusercontent.com/u/179229932']
+}
+
+// Create the modal
+createAppKit({
+  adapters: [wagmiAdapter],
+  projectId,
+  networks: [polygon],
+  defaultNetwork: polygon,
+  metadata: metadata,
+  features: {
+    analytics: true
+  }
+})
+
+function Providers({ children, cookies }: { children: ReactNode; cookies: string | null }) {
+  const initialState = cookieToInitialState(wagmiAdapter.wagmiConfig as Config, cookies)
+
+  return (
+    <WagmiProvider config={wagmiAdapter.wagmiConfig as Config} initialState={initialState}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+export default Providers
